7388ed5e7f6afa05ce4dfbae574cad9b9aa42ae0
Paramesh Chandra <51922459+paramphy@users.noreply.github.com>
2020-12-19 11:22:37 +0530

    update

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import utlis
 from matplotlib import pyplot as plt
 import tkinter as tk
 
-
-
 #Defining Empty
 def empty(a):
     pass
@@ -42,7 +40,7 @@ def getContours(img,filter,minArea):
             cv2.putText(imgContour, str(area), (x + w - 100, y + h - 200),
                         cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
             print('area',arealist)
-            print('recarea',recarealist)
+            print('rectangle_area',recarealist)
 
 #Defining Trackbars
 cv2.namedWindow("TrackBars")
@@ -56,7 +54,7 @@ cv2.createTrackbar("Minimum Area","TrackBars",1000,10000,empty)
 
 
 while True:
-    img = cv2.imread("3.JPG")
+    img = cv2.imread("film.png")
     imgContour = img.copy()
     cx = cv2.getTrackbarPos("Canny X", "TrackBars")
     cy = cv2.getTrackbarPos("Canny Y","TrackBars")
@@ -76,7 +74,7 @@ while True:
     imageContour = getContours(imgErode,4,minArea)
     #print(imgContour)
 
-    imgStack = utlis.stackImages(.15, ([img,imgThres, imgContour]))
+    imgStack = utlis.stackImages(.4, ([img,imgThres, imgContour]))
     imgContour = cv2.resize(imgContour,(0,0),None,.5,.5)
 
     cv2.imshow("Image Stacks", imgStack)
